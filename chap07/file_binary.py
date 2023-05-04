563bc0e1c2091a9fa36fa132b86519611cc13607
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-05-04 20:25:21 +0900

    7.2まで学習完了

---
+++ chap07/file_binary.py
@@ -0,0 +1,3 @@
+with open('./chap07/input.png', 'rb') as reader, open('./chap07/output.png', 'wb') as writer:
+    while d := reader.read(1):
+        writer.write(d)
---
+++ chap07/file_readlines.py
@@ -0,0 +1,5 @@
+with open('./chap07/sample.txt', 'r', encoding='UTF-8') as file:
+    data = file.readlines()
+
+for line in data:
+    print(line, end='')
