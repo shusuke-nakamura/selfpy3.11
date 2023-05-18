533547f6a504a4c3089ed4b2ea5ef11573e7ecc8
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-05-18 23:02:22 +0900

    chap10の学習開始

---
+++ chap10/class_basic.py
@@ -0,0 +1,7 @@
+class Person:
+    pass
+
+
+if __name__ == '__main__':
+    p = Person()
+    print(p)
---
+++ chap10/class_method.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, firstname, lastname):
+        self.fristname = firstname
+        self.lastname = lastname
+
+    def show(self):
+        print(f'私の名前は{self.lastname}{self.fristname}です！')
+
+
+if __name__ == '__main__':
+    p = Person('太郎', '山田')
+    p.show()
