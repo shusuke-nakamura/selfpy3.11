90ac3e329838f99f4f2ca2f6c4e5eedaf4ecb65e
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-03 19:41:13 +0900

    11.2まで学習完了

---
+++ chap11/reserve_compare.py
@@ -0,0 +1,34 @@
+class Conrdinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # <
+    def __lt__(self, other):
+        return self.x ** 2 + self.y ** 2 < other.x ** 2 + other.y ** 2
+
+    # <=
+    def __le__(self, other):
+        return self.x ** 2 + self.y ** 2 <= other.x ** 2 + other.y ** 2
+
+    # >
+    def __gt__(self, other):
+        return not self.__le__(other)
+
+    # >=
+
+    def __ge__(self, other):
+        return not self.__lt__(other)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+
+if __name__ == '__main__':
+    c1 = Conrdinate(1, 2)
+    c2 = Conrdinate(15, 25)
+    c3 = Conrdinate(2, 1)
+    print(c1 < c2)
+    print(c1 <= c3)
+    print(c1 <= c2)
+    print(c1 > c2)
