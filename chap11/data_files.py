96ac187f1ab7407bdb148b028134d09b961f2bb1
Shusuke Nakamura <shusuke.pana@gmail.com>
2023-06-10 16:33:38 +0900

    11.3まで学習完了

---
+++ chap11/data_files.py
@@ -0,0 +1,12 @@
+import dataclasses
+
+
+@dataclasses.dataclass()
+class Person:
+    firstname: str
+    lastname: str
+
+
+if __name__ == '__main__':
+    for f in dataclasses.fields(Person):
+        print(f)
---
+++ chap11/practice/p_dataclasses.py
@@ -0,0 +1,15 @@
+import dataclasses
+
+
+@dataclasses.dataclass()
+class Hamster:
+    name: str = '名無し'
+    age: int = 0
+
+    def show(self):
+        print(f'{self.name}は{self.age}歳です!')
+
+
+if __name__ == '__main__':
+    h = Hamster('のどか', 1)
+    h.show()
